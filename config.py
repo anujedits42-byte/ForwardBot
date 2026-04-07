2269b4883aee130dcfc7d4060f1fe5c079a9c089
anujedits42-byte <anujedits42@gmail.com>
2026-04-07 12:43:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import os
 from os import environ
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "AutoForwardX") 
+    API_ID = environ.get("API_ID", "34446649")
+    API_HASH = environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8564884831:AAF0ksogvIew4CDv5N8HcSJOq-vkCrrAOHk") 
+    BOT_SESSION = environ.get("BOT_SESSION", "encoding_ak_bot") 
     
     # FIX: Checks both common names for the MongoDB string
-    DATABASE_URI = environ.get("DATABASE_URI", environ.get("DATABASE", ""))
-    DATABASE_NAME = environ.get("DATABASE_NAME", "ForwardBot")
+    DATABASE_URI = environ.get("DATABASE_URI", environ.get("DATABASE", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0"))
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Anujedit")
     
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8496419402').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003656791142'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "THEUPDATEDGUYS") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7892805795').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003515041061'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "log_channel_a") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     
     # Ensure PORT is an integer for Gunicorn/Web server
-    PORT = int(environ.get('PORT', '8080'))
+    PORT = int(environ.get('PORT', '5000'))
 
 class temp(object): 
     lock = {}
